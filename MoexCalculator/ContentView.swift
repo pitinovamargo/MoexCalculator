e97319374afe23644baad305970216681de0d992
Margarita <pitinovamargo@gmail.com>
2024-04-03 21:25:34 +0200

    Draft

--- MoexCalculator/ContentView.swift
+++ MoexCalculator/ContentView.swift
@@ -8,23 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var color = Color.green
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Привет, Практикум!")
+                .padding()
+                .foregroundColor(color)      // 2
+            Button("Нажми здесь!") {         // 3
+                if color == .green {
+                    color = .orange
+                } else {
+                    color = .green
+                }
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
     ContentView()
+      .previewDevice("iPhone 14 Pro")
+  }
 }
-
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
